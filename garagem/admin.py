d937c1db15495fd52832923cedbcc12da5dfe4cb
PauloK3tchup <passandoalimpo13@gmail.com>
2023-03-10 16:28:35 -0300

    Criação do modelo Categoria

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from garagem.models import Categoria, Marca
+
+admin.site.register(Categoria)
+admin.site.register(Marca)
