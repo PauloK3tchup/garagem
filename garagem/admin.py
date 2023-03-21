8d81c4906b8e0aeeb99ad2023c36498ed6515e9a
PauloK3tchup <PauloK3tchup>
2023-03-21 15:13:34 -0300

    Adição de Cores, Acessórios e Veículos

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from garagem.models import Categoria, Marca
+from garagem.models import Categoria, Marca, Cor, Acessorio, Veiculo
 
 admin.site.register(Categoria)
 admin.site.register(Marca)
+admin.site.register(Cor)
+admin.site.register(Acessorio)
+admin.site.register(Veiculo)
--- garagem/models.py
+++ garagem/models.py
@@ -14,3 +14,30 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Cor(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+    class Meta:
+        verbose_name_plural = "Cores"
+
+class Acessorio(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+    
+class Veiculo(models.Model):
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    cor = models.ForeignKey(Cor, on_delete=models.CASCADE)
+    acessorios = models.ForeignKey(Acessorio, on_delete=models.CASCADE)
+    modelo = models.CharField(max_length=100)
+    ano = models.IntegerField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.modelo
