5ac6aeeb4406977e5ddeb972e9452c17da90e53e
PauloK3tchup <passandoalimpo13@gmail.com>
2023-04-18 14:29:32 -0300

    Api para as demais informações

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,14 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from garagem.views import MarcaViewSet
+from garagem.views import MarcaViewSet, CategoriaViewSet, CorViewSet, AcessorioViewSet, VeiculoViewSet
 
 router = DefaultRouter()
 router.register(r"Marca", MarcaViewSet)
+router.register(r"Categoria", CategoriaViewSet)
+router.register(r"Cor", CorViewSet)
+router.register(r"Acessorio", AcessorioViewSet)
+router.register(r"Veiculo", VeiculoViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
