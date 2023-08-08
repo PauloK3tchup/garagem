6dd5b6736c4f9c50ecef96cdeed8a9c0130ef191
PauloK3tchup <passandoalimpo13@gmail.com>
2023-08-08 13:29:13 -0300

    Conta e token

---
+++ garagem/views/__init__.py
@@ -0,0 +1,6 @@
+from .acessorio import AcessorioViewSet
+from .categoria import CategoriaViewSet
+from .marca import MarcaViewSet
+from .modelo import ModeloViewSet
+from .veiculo import VeiculoViewSet
+from .cor import CorViewSet
